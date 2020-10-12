e1b32ebf277d10e2a3662ad625b3027bdc1efb27
杉山春輝 <sugiyamaharuki@sugiyamaharuakiranoMacBook-Pro.local>
2020-10-12 18:22:01 +0900

    add buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
 const App = () => {
-  return <div>App Hi!</div>;
+  return (
+    <div>
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="eight wide column">
+            <SongList />
+          </div>
+          <div className="eight wide column">
+            <SongDetail />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
